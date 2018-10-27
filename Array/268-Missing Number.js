a43762d902f5ce3cb06746bebb33f339a6ebc201
LuckyAbby <luckyabby97@gmail.com>
2018-10-27 21:52:34 +0800

    add missing number

---
+++ Array/268-Missing Number.js
@@ -0,0 +1,25 @@
+/**
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let res = nums.length * (nums.length + 1) / 2;
+    nums.map(num => res = res - num);
+    return res;
+    
+};
